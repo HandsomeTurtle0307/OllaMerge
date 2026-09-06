a5ea6d4bd69d4df3da8662aca00a64eb97121b63
HandsomeTurtle0307 <shengyq0307@outlook.com>
2026-09-06 15:50:28 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
-#include "mainwindow.h"
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2026 HandsomeTurtle0307
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ */
+include "mainwindow.h"
 
 #include <QApplication>
 
