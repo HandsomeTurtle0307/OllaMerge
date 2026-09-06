ef002e8f668125581bb874e11db26cbd11f76eae
HandsomeTurtle0307 <shengyq0307@outlook.com>
2026-09-06 15:50:51 +0800

    Update mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,3 +1,11 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2026 HandsomeTurtle0307
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ */
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
