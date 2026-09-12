1558010cb53156cbcce027abd757eaadea6fffe6
HandsomeTurtle0307 <shengyq0307@outlook.com>
2026-09-12 13:55:49 +0800

    Restore license comments and include directives

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License.
  */
-include "mainwindow.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
